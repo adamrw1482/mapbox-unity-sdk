2fcd8d933af7d829280aa272435c332f9b3b0f72
Baran Kahyaoglu <baran.kahyaoglu@mapbox.com>
2025-12-10 00:42:35 +0300

    change tile creator to support non-instanced mat

--- Runtime/Mapbox/BaseModule/Unity/ModuleBehaviours/TileCreatorBehaviour.cs
+++ Runtime/Mapbox/BaseModule/Unity/ModuleBehaviours/TileCreatorBehaviour.cs
@@ -15,13 +15,14 @@ namespace Mapbox.Example.Scripts.ModuleBehaviours
         [Tooltip("Materials for base map tile mesh and gameobject")]
         public Material[] TileMaterials;
 
+        public bool InstanceMaterials = true;
         public int CacheSize = 25;
     
         public ITileCreator GetTileCreator(UnityContext unityContext)
         {
             if (_tileCreator != null) return _tileCreator;
 
-            _tileCreator = new TileCreator(unityContext, TileMaterials, CacheSize);
+            _tileCreator = new TileCreator(unityContext, TileMaterials, CacheSize, InstanceMaterials);
             return _tileCreator;
         }
     }
